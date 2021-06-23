c2a2e6590ddf41eb3d8e6a6c9d9d55c5db61ce50
jigyasunayyar <85394895+jigyasunayyar@users.noreply.github.com>
2021-06-23 18:12:53 +0530

    Update unittestcases.py

--- unittestcases.py
+++ unittestcases.py
@@ -19,7 +19,7 @@ class TestMethods(unittest.TestCase):
         self.assertEqual(methods.multiply(4,5), 20)
         self.assertEqual(methods.multiply(-3,4,-1), 12)
         self.assertEqual(methods.multiply(1000,0,-10), 0)
-        self.assertEqual(methods.multiply(1000,0,-10), 0)
+#         self.assertEqual(methods.multiply(1000,0,-10), 0)
        
     def test_pylint(self):
         results = lint.Run(['codefile/methods.py'],do_exit=False)
