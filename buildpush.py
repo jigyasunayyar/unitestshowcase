0dab731bda7eae65fd53c01b2416f9bd5d540cbd
jigyasunayyar <85394895+jigyasunayyar@users.noreply.github.com>
2021-06-23 15:33:02 +0530

    Update buildpush.py

--- buildpush.py
+++ buildpush.py
@@ -1,8 +1,12 @@
 import os
-print('exporting the path to path variable')
-os.system('export PATH=/builder/home/.local/bin:$PATH >> ~/.bashrc')
-os.system('. ~/.bashrc')
+# print('exporting the path to path variable')
+# os.system('export PATH=/builder/home/.local/bin:$PATH >> ~/.bashrc')
+# os.system('. ~/.bashrc')
 
-print('pushing the repo using twine')
-os.system('twine upload --repository-url https://console.cloud.google.com/artifacts/pypi/mytest-316909/asia-south1/unittest-repo/ dist/*')
+# print('pushing the repo using twine')
+# os.system('twine upload --repository-url https://console.cloud.google.com/artifacts/pypi/mytest-316909/asia-south1/unittest-repo/ dist/*')
+
+print('copy the file to user home folder')
+os.system('cp .pypirc ~/)
+
           
